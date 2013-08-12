ed6acc1ffccd1231b55d3386bcbd06e0bef220a1
adnan <mail@adnanwahab.com>
2013-08-11 23:54:54 -0700

    firsrt pass

---
+++ main.js
@@ -0,0 +1,142 @@
+//stroke
+  // thickness
+  // dasharray
+  // length
+  // opacity
+  // color
+//fill
+  // pattern
+  // gradient + shaders
+  // color
+  // opacity
+
+
+var pathgl = {}
+  , pmatrix = [0.0031446540880503146, 0, 0, 0, 0, 0.004, 0, 0, 0, 0, -1, 0, -1, -1, 0, 1]
+  , log = console.log.bind(console)
+  , canvas, ctx, program, pos
+
+var actions = { m: moveTo
+              , z: closePath
+              , l: lineTo
+
+              , h: 'horizontalLine'
+              , v: 'verticalLine'
+              , c: 'curveTo'
+              , s: 'shortCurveTo'
+              , q: 'quadraticBezier'
+              , t: 'smoothQuadraticBezier'
+              , a: 'elipticalArc'
+              }
+
+pathgl.fragment = [ "precision mediump float;"
+           , "void main(void) {"
+           , "gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"
+           , "}"
+           ].join('\n')
+
+pathgl.vertex = [ "attribute vec3 aVertexPosition;"
+           , "uniform mat4 uPMatrix;"
+           , "void main(void) {"
+           , "gl_Position = uPMatrix * vec4(aVertexPosition, 1.0);"
+           , "}"
+           ].join('\n')
+
+pathgl.init = init
+pathgl.draw = draw
+
+function draw (str) {
+  var split = str.split(/([A-Za-z])/)
+              .map(function(d) { return d.trim().toLowerCase() })
+              .filter(function(d) { return d })
+    , i = 0, action, coords, j
+  while (i < split.length) {
+    action = actions[split[i++]]
+    coords = split[i++] || ''
+    if (! action.bind) throw new Error(action + ' ' + split[i - 1] + ' is not yet implemented')
+    coords ? twoEach(coords.split(' '), action) : action(coords)
+  }
+}
+
+function moveTo(x, y) {
+  pos = [x, canvas.height - y]
+}
+
+function closePath() {
+  render()
+}
+
+function lineTo(x, y) {
+  addLine.apply(0, pos.concat(pos = [x, canvas.height - y]))
+}
+
+function compileShader (type, src) {
+  var shader = ctx.createShader(type)
+  ctx.shaderSource(shader, src)
+  ctx.compileShader(shader)
+  if (! ctx.getShaderParameter(shader, ctx.COMPILE_STATUS)) throw new Error(ctx.getShaderInfoLog(shader))
+  return shader
+}
+
+function initShaders() {
+  var vertexShader = compileShader(ctx.VERTEX_SHADER, pathgl.vertex)
+  var fragmentShader = compileShader(ctx.FRAGMENT_SHADER, pathgl.fragment)
+  program = ctx.createProgram()
+  ctx.attachShader(program, vertexShader)
+  ctx.attachShader(program, fragmentShader)
+  ctx.linkProgram(program)
+
+  if (! ctx.getProgramParameter(program, ctx.LINK_STATUS)) return log("Shader is broken")
+
+  ctx.useProgram(program)
+
+  program.vertexPositionLoc = ctx.getAttribLocation(program, "aVertexPosition")
+  ctx.enableVertexAttribArray(program.vertexPositionLoc)
+
+  program.pMatrixLoc = ctx.getUniformLocation(program, "uPMatrix")
+}
+
+var lineBuffers = [];
+
+function addLine(x1, y1, x2, y2) {
+  var index = lineBuffers.push(ctx.createBuffer()) - 1
+  var vertices = [x1, y1, 0, x2, y2, 0]
+  ctx.bindBuffer(ctx.ARRAY_BUFFER, lineBuffers[index])
+  ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(vertices), ctx.STATIC_DRAW)
+
+  lineBuffers[index].itemSize = 3
+  lineBuffers[index].numItems = vertices.length / 3
+}
+
+function render() {
+  ctx.clear(ctx.COLOR_BUFFER_BIT)
+
+  ctx.uniformMatrix4fv(program.pMatrixLoc, 0, pmatrix)
+
+  for (var i = 0; i < lineBuffers.length; i++) {
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, lineBuffers[i])
+    ctx.vertexAttribPointer(program.vertexPositionLoc, lineBuffers[i].itemSize, ctx.FLOAT, false, 0, 0)
+    ctx.drawArrays(ctx.LINE_STRIP, 0, lineBuffers[i].numItems)
+  }
+}
+
+function initContext() {
+  canvas = document.querySelector("canvas")
+  ctx = canvas.getContext('webgl', { antialias: false })
+  pos = [0, canvas.height]
+
+  ctx.clearColor(0.0, 0.0, 0.0, 1.0)
+  ctx.viewportWidth = canvas.width
+  ctx.viewportHeight = canvas.height
+}
+
+function init() {
+  initContext()
+  initShaders()
+  return pathgl
+}
+
+function twoEach(list, fn) {
+  var l = list.length - 1, i = 0
+  while(i < l) fn(list[i++], list[i++])
+}
---
+++ test/test.js
@@ -0,0 +1,27 @@
+var get = document.querySelector.bind(document)
+  , canvas = get('canvas')
+  , svg = get('svg')
+
+  , width = canvas.height = svg.style.height = 500
+  , height = canvas.width = svg.style.width = innerWidth * .499
+
+function appendPath(d) {
+  var path = document.createElement('path')
+  svg.appendChild(path)
+  path.d = d
+}
+
+var paths = [ 'm 0 0 l 10 10 60 60 70 400 z'
+            , 'm 50 60 l 60 50 50 60 40 50 50 40 60 50 z'
+            ].map(function (d) { return d.toUpperCase() })
+
+function draw(d) {
+  d3.select('svg').append('path')
+  .attr('d', d)
+  .attr('stroke', '#333')
+  .attr('fill', 'none')
+}
+
+
+paths.forEach(draw)
+paths.forEach(pathgl.init().draw)
