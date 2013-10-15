ab2d873aa8f792aec6a51cbf7cb63535cc895e14
Adnan Wahab <mail@adnanwahab.com>
2013-10-14 20:22:23 -0700

    add build

---
+++ build.js
@@ -0,0 +1,16 @@
+var compressor = require('uglify-js')
+  , fs = require('fs')
+  , source = './src/'
+  , orig = fs.readdirSync(source)
+           .map(concat)
+           .join('')
+
+fs.writeFileSync('pathgl.js', orig)
+
+fs.writeFileSync('pathgl.min.js',
+                 compressor.minify(orig, { fromString: true }).code
+                )
+
+function concat (file) {
+  return '' + fs.readFileSync(source + file)
+}
